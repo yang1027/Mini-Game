b8752d72497269241389ef67e100971ad0bf2dc8
杨 <874081235@qq.com>
2019-04-15 13:59:01 +0800

    添加项目文件。

---
+++ Practice/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+            
+        {
+            Console.WriteLine("Hello  world!");
+
+            //int[] array = new int[] { 5, 3, 2, 6, 7, 1, 9, 4, 8 };
+            //Console.WriteLine($"数组: 5, 3, 2, 6, 7, 1, 9, 4, 8");
+            //int max = 0;
+            //Console.WriteLine($"设最大初始值为：{max}");
+            //for (int i = 0; i <array .Length ; i++)
+            //{
+            //    if (array[i] > max)
+            //    {
+            //        Console.WriteLine($"第{i}次比较，{array[i]}>{max},此时最大值为：{array[i]}");
+            //        max = array[i];
+
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"第{i}次比较，{array[i]}<{max },此时最大值为：{max }");
+            //    }
+            //}
+
+            //Console.WriteLine($"最大值为：{+max}");
+
+
+            string what = Console.ReadLine();
+
+            //Console.Read();
+            
+
+        }
+    }
+}
