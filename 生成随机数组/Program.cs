7ab5fb3da93db8dfc6cec353d52453796fec0086
杨 <874081235@qq.com>
2019-04-18 18:44:16 +0800

    生成随机数组

---
+++ 生成随机数组/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace 生成随机数组
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine($"生成一个元素从小到大的随机数组：");
+
+            int Length = 10;
+            int[] array = new int[Length];
+            array[0] = 7;
+            Console.Write(array[0] + " ");
+
+            for (int i = 1; i < Length; i++)
+            {
+                array[i] = array[i - 1] + new Random().Next(1,5);
+
+                Console.Write(array[i]+" ");
+
+               // Console.WriteLine(new Random().Next (1,5));
+
+            }
+
+
+        }
+    }
+}
